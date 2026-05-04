060e5fa4db38eb1c8278d9fae33526fcb271d3b8
lucasprietoalmeida <lucasprietoalmeidaan@gmail.com>
2026-05-04 12:30:19 +0200

    fix: remove duplicated BrowserRouter to avoid nested routing error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
 
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
+        <App />
 
-                <Toaster
-                    position="top-right"
-                    toastOptions={{
-                        duration: 3000,
-                        style: {
-                            borderRadius: "8px",
-                            padding: "12px",
-                        },
-                    }}
-                />
-            </AuthProvider>
-        </BrowserRouter>
+        <Toaster
+            position="top-right"
+            toastOptions={{
+                duration: 3000,
+                style: {
+                    borderRadius: "8px",
+                    padding: "12px",
+                },
+            }}
+        />
     </React.StrictMode>
 );
